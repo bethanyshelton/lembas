ac8b9691f376b2d2fc01a1a815f3a196db4c8272
Bethany Shelton <59832223+bethanyshelton@users.noreply.github.com>
2021-02-16 07:40:12 -0500

    Setup basic file structure, npm & express

---
+++ app.js
@@ -0,0 +1,11 @@
+// express
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log(`Server started at port 3000`);
+});
