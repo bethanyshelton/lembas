013deaa46fc76dabe578fc0f1c5c0e66ae6a6152
Bethany Shelton <59832223+bethanyshelton@users.noreply.github.com>
2021-02-17 16:52:09 -0500

    Set up ejs & add pages and partials

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-// express
-const express = require('express');
-const app = express();
+// require dependencies
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// home route
+
+app.get("/", (req, res) => {
+	res.render("pages/home");
 });
 
 app.listen(3000, () => {
-  console.log(`Server started at port 3000`);
+	console.log(`Server started at port 3000`);
 });
